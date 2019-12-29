ee3ba45cc8fdd80064d103e5dfca3e0d384b380b
Simon Tucker <sjtucker28@gmail.com>
2019-12-29 17:31:37 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(sentence)
-  new_array = sentence.split("")
+  new_array = sentence.split(" ")
   reversed = new_array.each do {|word| word.reverse}
   return reversed.join(" ")
 end
